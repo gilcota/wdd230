7569d952814818c5559f810e01db79ebd8b8070d
gilcota <gilemmmc@hotmail.com>
2023-09-28 09:04:46 -0600

    L03: Home Page Enhancement

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,11 @@ document.querySelector('#currentYear').textContent = currentDate.getFullYear();
 
 const lastModified = new Date();
 document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
