0026d6b2b3aa0517e29e70714b80cda9aa4f067b
gilcota <gilemmmc@hotmail.com>
2023-10-16 23:02:03 -0600

    L05: Chamber Site Dark Mode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,5 +1,6 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
+const fontcolor = document.getElementById("member-title");
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
@@ -13,10 +14,13 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+        fontcolor.style.color = "white";
+
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
+        fontcolor.style.color = "#2D2177";
     }
 }
 
