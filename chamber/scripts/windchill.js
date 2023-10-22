484322773f6633913b2abab38bd212c596b1a346
gilcota <gilemmmc@hotmail.com>
2023-10-22 10:57:49 -0600

    06: Chamber Home - Responsive Images

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -4,10 +4,11 @@ const windspeed = parseFloat(document.querySelector("#windspeed").textContent)
 var windchill = "N/A";
 
 if (temperature <= 50 && windspeed > 3) {
-    windchill = (35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temperature * Math.pow(windspeed, 0.16))).toFixed(1);
+    windchill = (35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temperature * Math.pow(windspeed, 0.16))).toFixed(1) + " °F"
 } else {
     windchill = "N/A";
 }
+
 
 document.querySelector("#windchill").textContent = windchill;
 
