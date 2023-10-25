ac23005ff602fe8808911a89a31918baeefb45ff
gilcota <gilemmmc@hotmail.com>
2023-10-24 22:46:37 -0600

    L07: Lazy Loading

---
+++ lesson03/scripts/lastmodified.js
@@ -0,0 +1,5 @@
+// const currentDate = new Date();
+// document.querySelector('#currentYear').textContent = currentDate.getFullYear();
+
+const lastModified = new Date();
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
