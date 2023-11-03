ff87ceda29670fe3f1a1ff26a8cb881b9ed44268
gilcota <gilemmmc@hotmail.com>
2023-11-03 13:45:13 -0600

    L08: Forms

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,19 @@
+const currentDate = new Date();
+document.querySelector('#currentYear').textContent = currentDate.getFullYear();
+
+const lastModified = new Date();
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const herolabel = document.querySelector('.herolabel');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+
 const kp1 = document.querySelector("#password");
 const kp2 = document.querySelector("#passwordr");
 const message = document.querySelector("#formmessage");
