6e1b797d564870945895247d4a03bd66666368c2
gilcota <gilemmmc@hotmail.com>
2023-11-07 21:31:48 -0600

    L09: Fetch API

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -30,8 +30,8 @@ const displayProphets = (prophets) => {
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
         portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+        portrait.setAttribute('width', '280');
+        portrait.setAttribute('height', '362');
 
 
 
