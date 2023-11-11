9fb49d9b522d46726bb65eb83b7811b3762e7d88
gilcota <gilemmmc@hotmail.com>
2023-11-10 21:28:48 -0600

    L09: Directory

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -15,33 +15,30 @@ const displayMembers = (members) => {
     members.forEach((member) => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
-        let fullName = document.createElement('h2'); // fill in the blank
-        let dateBirth = document.createElement('p');
-        let placeBirth = document.createElement('p');
-        let portrait = document.createElement('img');
+        let logo = document.createElement('img');
+        let address = document.createElement('p');
+        let phonenumber = document.createElement('p');
+        const site = document.createElement('a');
 
 
-        // Build the h2 content out to show the member's full name
-        fullName.textContent = `${member.name} ${member.lastname}`; // fill in the blank
-
-        dateBirth.textContent = `Date of birth: ${member.birthdate}`
-        placeBirth.textContent = `Place of birth: ${member.birthplace}`
-
-        // Build the image portrait by setting all the relevant attributes
-        portrait.setAttribute('src', member.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${member.name} ${member.lastname}`); // fill in the blank
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '280');
-        portrait.setAttribute('height', '362');
 
 
+        address.textContent = `${member.address}`;
+        phonenumber.textContent = `${member.phonenumber}`;
+        site.textContent = `${member.websiteurl}`;
+        site.setAttribute('href', member.websiteurl);
+
+        // Build the image logo by setting all the relevant attributes
+        logo.setAttribute('src', baseURL + member.logo);
+        logo.setAttribute('alt', `Logo of ${member.name}`); // fill in the blank
+        logo.setAttribute('loading', 'lazy');
 
         // Append the section(card) with the created elements
-        card.appendChild(fullName); //fill in the blank
-        card.appendChild(dateBirth);
-        card.appendChild(placeBirth);
+        card.appendChild(logo); //fill in the blank
+        card.appendChild(address);
+        card.appendChild(phonenumber);
+        card.appendChild(site);
 
-        card.appendChild(portrait);
 
 
         cards.appendChild(card);
