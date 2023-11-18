305a6ce16e87dea0302eb699c5e23833c8624f43
gilcota <gilemmmc@hotmail.com>
2023-11-17 18:28:52 -0600

    L10: Collaboration Activity - Weather API

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,7 +2,7 @@
 // select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const captionDesc = document.querySelector('#figcaption');
 
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=20.55&lon=-100.44&units=metric&appid=155931a8ab93edc39d34b1516aad0728';
