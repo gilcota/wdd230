74de1e771f63338aa23b7643a682e6771e00bc64
gilcota <gilemmmc@hotmail.com>
2023-11-21 19:17:16 -0600

    L10: Chamber Project - Home Page Enhancements

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -11,3 +11,28 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+
+
+//Invitation banner
+const invitationBanner = document.querySelector(".invbanner")
+const d = new Date();
+let day = d.getDay();
+console.log(day);
+
+if (day == 4 | day == 5 | day == 6 | day == 7) {
+    console.log("Today is Mon-Wed")
+    invitationBanner.style.display = 'none';
+};
+
+//Invitation banner close button
+// Get all elements with class="close"
+var closebtns = document.getElementsByClassName("close");
+var i;
+
+// Loop through the elements, and hide the parent, when clicked on
+for (i = 0; i < closebtns.length; i++) {
+    closebtns[i].addEventListener("click", function () {
+        this.parentElement.style.display = 'none';
+    });
+}
