af540e5c34a877dd30bb80785de64b04504d5568
gilcota <gilemmmc@hotmail.com>
2023-11-28 00:39:58 -0500

    L11: Chamber Final

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,88 +1,3 @@
-const url = "https://gilcota.github.io/wdd230/chamber/data/members.json";
-const baseURL = "https://gilcota.github.io/wdd230/chamber/";
-const grid = document.querySelector('.spotlights');
-
-async function getMemberData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    //console.table(data.members);
-    displayMembers(data.members); // note that we reference the members array of the JSON data object, not just the object
-}
-
-getMemberData();
-
-const displayMembers = (members) => {
-    members.forEach((member) => {
-        // Create elements to add to the div.cards element
-        let card = document.createElement('section');
-        let logo = document.createElement('img');
-        let address = document.createElement('p');
-        let phonenumber = document.createElement('p');
-        const site = document.createElement('a');
-        let memberlevel = document.createElement('p');
-        let companycontact = document.createElement('p');
-        let name = document.createElement('p');
-
-
-
-
-        address.textContent = `${member.address}`;
-        phonenumber.textContent = `${member.phonenumber}`;
-        site.textContent = `${member.websiteurl}`;
-        site.setAttribute('href', member.websiteurl);
-        memberlevel.textContent = `${member.level} Member`;
-        companycontact.textContent = `Contact: ${member.contactname} ${member.contactlastname}`;
-        name.textContent = `${member.name}`
-
-        // Build the image logo by setting all the relevant attributes
-        logo.setAttribute('src', baseURL + member.logo);
-        logo.setAttribute('alt', `Logo of ${member.name}`); // fill in the blank
-        logo.setAttribute('loading', 'lazy');
-
-        card.setAttribute('class', "sectiondir")
-        logo.setAttribute('class', "imgdir")
-        site.setAttribute('target', "_blank")
-        name.setAttribute('class', "compname")
-        memberlevel.setAttribute('class', "mlevel")
-        companycontact.setAttribute('class', "ccontact")
-
-        // Append the section(card) with the created elements
-        card.appendChild(logo); //fill in the blank
-        card.appendChild(name);
-        card.appendChild(memberlevel);
-        card.appendChild(address);
-        card.appendChild(phonenumber);
-        card.appendChild(companycontact);
-        card.appendChild(site);
-
-
-
-
-
-        grid.appendChild(card);
-    }); // end of arrow function and forEach loop
-}
-
-
-//------------LAYOUT VIEW SELECTION SECTION---------------//
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
-
-gridbutton.addEventListener("click", () => {
-    // example using arrow function
-    display.classList.add("grid");
-    display.classList.remove("list");
-});
-
-listbutton.addEventListener("click", showList); // example using defined function
-
-function showList() {
-    display.classList.add("list");
-    display.classList.remove("grid");
-}
 
 
 
@@ -114,44 +29,46 @@ function showList() {
 
 document.addEventListener("DOMContentLoaded", () => {
     // Create spotlight section
-    spotlight_json().then(business => {
-        // Display the spotlight business
-        spotlight_show(business);
+    spotlight_json().then(name => {
+        // Display the spotlight name
+        spotlight_show(name);
     });
 });
 
 /**
  * Get spotlight data from JSON file
- * @returns Array with business data
+ * @returns Array with name data
  */
 async function spotlight_json() {
-    // Spotlight business array
-    let business = [];
+    // Spotlight name array
+    let name = [];
+    // console.log(name);
     // Fetch json data
-    const response = await fetch("json/directory.json");
+    const response = await fetch("data/members.json");
     // Verify response
     if (response.ok) {
         // Wait for data
         const data = await response.json();
         // Filter all gold and silver members
-        const filter = data.directory.filter(business => {
+        console.table(data.members);
+        const filter = data.members.filter(name => {
             // Return when membership is silver or gold
-            if (business.membership === "silver" || business.membership === "gold") {
+            if (name.membership === "Silver" || name.membership === "Gold") {
                 return true;
             }
         });
-        // Randomly choose three business
+        // Randomly choose three name
         for (let i = 0; i < 3; i++) {
             // Get random index
             const index = random_index(filter);
-            // Add business data
-            business.push(filter[index]);
-            // Remove selected business
+            // Add name data
+            name.push(filter[index]);
+            // Remove selected name
             filter.splice(index, 1);
         }
     }
     // Return array
-    return business;
+    return name;
 }
 
 /**
@@ -166,33 +83,33 @@ function random_index(arr) {
 
 /**
  * Displays spotlight from Array
- * @param {Array} business Array with business data
+ * @param {Array} name Array with name data
  */
-function spotlight_show(business) {
+function spotlight_show(name) {
     // Get spotlights
-    const spotlights = document.querySelectorAll(".spotlight");
+    const spotlights = document.querySelectorAll(".spotlights");
     // Create elements for every spotlight
-    business.forEach((json, index) => {
+    name.forEach((json, index) => {
         // Create elements
-        let businessName = document.createElement("h2");
-        let businessLogo = document.createElement("img");
+        let nameName = document.createElement("h2");
+        let nameLogo = document.createElement("img");
         let hr = document.createElement("hr");
-        let businessContact = document.createElement("p");
-        let businessAddress = document.createElement("p");
+        let nameContact = document.createElement("p");
+        let nameAddress = document.createElement("p");
         // Fill h2 with data
-        businessName.innerText = json.business;
+        nameName.innerText = json.name;
         // Fill img with data
-        businessLogo.setAttribute("src", json.logo);
-        businessLogo.setAttribute("alt", json.business);
+        nameLogo.setAttribute("src", json.logo);
+        nameLogo.setAttribute("alt", json.name);
         // Fill contact
-        businessContact.innerHTML = `<em>${json.phone} | ${json.email}</em>`;
+        nameContact.innerHTML = `<em>${json.phone} | ${json.email}</em>`;
         // Fill address
-        businessAddress.innerHTML = `<em>${json.address}</em>`;
+        nameAddress.innerHTML = `<em>${json.address}</em>`;
         // Append elements
-        spotlights[index].append(businessName);
-        spotlights[index].append(businessLogo);
+        spotlights[index].append(nameName);
+        spotlights[index].append(nameLogo);
         spotlights[index].append(hr);
-        spotlights[index].append(businessAddress);
-        spotlights[index].append(businessContact);
+        spotlights[index].append(nameAddress);
+        spotlights[index].append(nameContact);
     });
 }
